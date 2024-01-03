b327e87a6ff18a075daebeef0353bc061e9e7e6e
tiago-monteirox <tiagomonteirodev@outlook.com>
2024-01-02 23:00:05 -0300

    Model de Produto melhorado e verificação adicionada com o método clean(self)

--- lojapp/models.py
+++ lojapp/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils.text import slugify
 import os
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 class Categoria(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -31,15 +32,86 @@ class Categoria(models.Model):
         return reverse('', args=[self.slug])
     
 
+
+
 class Produto(models.Model):
     name = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE, null=True)
     quantidade = models.PositiveIntegerField(blank=True, null=True)
-    preco_venda = models.FloatField(verbose_name='Valor', blank=True, null=True)
-    descricao_curta = models.TextField(max_length=100,blank=True, null=True)
-    imagem = models.ImageField(
-        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    preco_venda = models.FloatField(verbose_name='Preço', blank=True, null=True)
+    preco_venda_promocional = models.FloatField(verbose_name='Preço promocional', blank=True, null=True)
+    descricao_curta = models.TextField(max_length=100, blank=True, null=True)
+    imagem = models.ImageField(upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
-    preco_custo = models.FloatField(verbose_name='Preço')
+    preco_custo = models.FloatField(verbose_name='Preço de custo')
 
     def __str__(self):
         return self.name
+
+    def clean(self):
+        # Validação dos preços para assegurar que não são negativos
+        if self.preco_venda is not None and self.preco_venda < 0:
+            raise ValidationError('O preço de venda não pode ser negativo.')
+        if self.preco_venda_promocional is not None and self.preco_venda_promocional < 0:
+            raise ValidationError('O preço promocional não pode ser negativo.')
+        if self.preco_custo is not None and self.preco_custo < 0:
+            raise ValidationError('O preço de custo não pode ser negativo.')
+
+        # Validação para assegurar que o preço promocional não é maior que o preço normal
+        if self.preco_venda_promocional is not None and self.preco_venda is not None:
+            if self.preco_venda_promocional > self.preco_venda:
+                raise ValidationError('O preço promocional não pode ser maior que o preço de venda.')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Chama o método clean para validação
+        super(Produto, self).save(*args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Produto(models.Model):
+#     name = models.CharField(max_length=100)
+#     quantidade = models.PositiveIntegerField(blank=True, null=True)
+#     preco_venda = models.FloatField(verbose_name='Valor', blank=True, null=True)
+#     descricao_curta = models.TextField(max_length=100,blank=True, null=True)
+#     imagem = models.ImageField(
+#         upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+#     slug = models.SlugField(unique=True, blank=True, null=True)
+#     preco_custo = models.FloatField(verbose_name='Preço')
+
+#     def __str__(self):
+#         return self.name
